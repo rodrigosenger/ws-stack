6ce26169d95f724a4d6b24406f2065ad965bbc78
rodrigo.senger <rodrigo.senger@wssim.com.br>
2018-03-26 15:00:38 -0300

    first commit

---
+++ test/specs/page-title.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+describe('fixture', () => {
+    it('has the expected page title', () => {
+        browser.url('/');
+        assert.equal(browser.getTitle(), 'End-to-End Testing');
+    });
+});
